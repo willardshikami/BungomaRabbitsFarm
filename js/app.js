e22f5d171ac7f80466d478b2779fd38e1649e5ae
willardshikami <willardshikami@gmail.com>
2016-11-23 15:15:46 +0300

    refreshed UI

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-$(document).foundation()
+
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
